e9488aa48719d580ea6932ba1f04d706ebc70a00
salo322 <salo.sulaberidze7@gmail.com>
2024-10-05 22:11:53 +0400

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  base: '/Lucky-Wheel/', // Replace with your repo name
-  plugins: [vue()],
-})
