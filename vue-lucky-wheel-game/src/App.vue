7d89898bcc769742dd122abb39922a00b12342e9
salo322 <salo.sulaberidze7@gmail.com>
2024-10-05 17:40:02 +0400

    Add files

---
+++ vue-lucky-wheel-game/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <LuckyWheel />
+  </div>
+</template>
+
+<script>
+import LuckyWheel from './components/LuckyWheel.vue';
+
+export default {
+  components: {
+    LuckyWheel
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #ECDFCC;
+}
+</style>
